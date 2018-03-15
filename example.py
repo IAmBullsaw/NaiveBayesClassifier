65cd0f705e094704bac632ee1002bc8eb5d2edcf
Oskar Jansson <oskja067@student.liu.se>
2018-03-15 11:35:55 +0100

    adds: example

---
+++ example.py
@@ -0,0 +1,21 @@
+from nbc import NBC
+import json
+import sys
+
+def read_data(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+def main(training, testing):
+    documents = read_data(training)
+    test_docs = read_data(testing)
+    nbc = NBC(documents)
+    print(nbc.num_tokens)
+    matrix = nbc.confusion_matrix(documents)
+    NBC.p_matrix(matrix)
+    matrix = nbc.confusion_matrix(test_docs)
+    NBC.p_matrix(matrix)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1], sys.argv[2])
